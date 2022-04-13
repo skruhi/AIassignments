5fd476d734ed6187745b99f8a1cd414347bb81aa
skruhi <98005870+skruhi@users.noreply.github.com>
2022-04-13 19:55:22 +0530

    Operation My Captain Task 2

---
+++ Project 2(Operations).py
@@ -0,0 +1,10 @@
+E ={0,2,4,6,8}
+N ={1,2,3,4,5}
+#Union of E and N
+print("Union of E and N is", E|N)
+#Intersection
+print("Intersection of E and N is", E&N)
+#Difference
+print("Difference of E and N is", E-N)
+#Symmetric Intersection
+print("Symmetric Intersection of E and N is", E^N)
